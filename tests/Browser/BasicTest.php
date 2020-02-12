3fb52951ba773df360d31d41ed93bd30a321cae4
lucian.stoicescu <lucian.stoicescu@Lucian-Stoicescus-MacBook-Pro.local>
2020-02-12 16:35:07 -0500

    ENG-3560 Almost done

--- tests/Browser/BasicTest.php
+++ tests/Browser/BasicTest.php
@@ -18,8 +18,13 @@ class BasicTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
                     ->assertSee('The Grommet')
-                    ->click('body > header > div.p-b-m.p-t-s-phone.p-b-s-phone > div > nav > ul > li:nth-child(8) > a')
-                    ->assertPathIs('*products*');
+                    //->click('div.wide-screen-rails.p-b-l.m-b-l > div:nth-child(3) > div.flex-8-12.flex-5-8-tablet > div.bg-beige.m-b-xl.m-b-0-phone.full-bleed-phone > div > div > div > a:nth-child(2)')
+                    //driver->findElement( WebDriverBy::xpath("//table[@class='x-grid3-row-table']/tbody/tr/td/div/a[contains(text(),'$value')]"))->click();
+                    ->clickAtXpath('//div[@class="tile-container-3 tile-container-2-mobile"]/a[2]')
+                    ->assertPathIs('*our-makers*')
+                    ->pause(5000)
+                    ->clickAtXpath('//div[@class="tile-container-3 tile-spacing-4 tile-container-2-tablet tile-spacing-2-mobile "]/a[2]')
+                    ->pause(5000);
         });
 
     }
