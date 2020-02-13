ec1bcc6281ee75a27b955e64f922f45d6c4f908d
LucianStoicescu <lucian.stoicescu@thegrommet.com>
2020-02-13 00:10:14 -0500

    ENV-3560 Got it to the last part

--- tests/Browser/BasicTest.php
+++ tests/Browser/BasicTest.php
@@ -22,9 +22,14 @@ class BasicTest extends DuskTestCase
                     //driver->findElement( WebDriverBy::xpath("//table[@class='x-grid3-row-table']/tbody/tr/td/div/a[contains(text(),'$value')]"))->click();
                     ->clickAtXpath('//div[@class="tile-container-3 tile-container-2-mobile"]/a[2]')
                     ->assertPathIs('*our-makers*')
-                    ->pause(5000)
+                    ->pause(3000)
                     ->clickAtXpath('//div[@class="tile-container-3 tile-spacing-4 tile-container-2-tablet tile-spacing-2-mobile "]/a[2]')
-                    ->pause(5000);
+                    ->pause(3000)
+                    ->clickAtXpath('//select/option[text()="3"]')
+                    ->pause(3000)
+                    ->clickAtXpath('//button[@formaction="cart.add"]')
+                    ->assertSee('3 items in your cart')
+                    ->pause(3000);
         });
 
     }
